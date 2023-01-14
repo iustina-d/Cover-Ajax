399640f8d6519b8c52f19e4867cd914a040893f8
iustina-d <iustina2dulea@gmail.com>
2023-01-14 02:36:37 +0200

    make with js the image to fade

--- js/home.js
+++ js/home.js
@@ -7,3 +7,20 @@ window.addEventListener("scroll", () => {
     navbar.classList.remove("bg")
   }
 })
+const collageImages = [...document.querySelectorAll('.collage-img')]
+
+collageImages.map((item,i)=>{
+  item.addEventListener('mouseover',()=>{
+    collageImages.map((image,index)=>{
+      if(index != i){
+        image.style.filter = 'blur(10px)';
+        item.style.zIndex =2;
+      }
+    })
+  })
+  item.addEventListener('mouseleave',()=>{
+    collageImages.map((image,index)=>{
+      image.style =null;
+    })
+  })
+})
